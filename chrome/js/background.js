fbe454ce181fd14d47bc0f92afb628d57d8f086f
Zsolt Nagy <nagy.zsolt.hun@gmail.com>
2014-08-04 08:30:11 +0200

    working on/off functionality

--- chrome/js/background.js
+++ chrome/js/background.js
@@ -3,30 +3,25 @@ var turnedOn = true;
 var iconDrawer = new IconDrawer();
 var ttsService = new GoogleTts();
 
-chrome.runtime.onMessage.addListener(
-	function(request, sender, sendResponse) {
-		switch(request.action) {
-			case("tts-read"):
-				if(! turnedOn) {return;}
-				var text = request.textToSpeech;
-				var lan = request.languageOfDocument || navigator.language;
-				ttsService.read(text,lan);
-				break;
-			case("tts-turnOnOff"):
-				if(turnedOn) {
-					turnedOn = false;
-					ttsService.stop();
-					iconDrawer.drawTurnedOff();
-					chrome.runtime.sendMessage({action: "tts-turnedOff"});
-				} else {
-					turnedOn = true;
-					iconDrawer.drawTurnedOn(0);
-					chrome.runtime.sendMessage({action: "tts-turnedOn"});
-				}
-				break;
-		}
-	}
-);
+function read(text, lan) {
+	if(! turnedOn) {return;}
+	ttsService.read(text,lan);
+}
+
+function turnOn() {
+	turnedOn = true;
+	iconDrawer.drawTurnedOn(0);
+	chrome.runtime.sendMessage({action: "tts-turnedOn"});
+	chrome.storage.local.set({'tts-status': 'on'}, function() {});
+}
+
+function turnOff() {
+	turnedOn = false;
+	ttsService.stop();	//in case it is reading, we stop it
+	iconDrawer.drawTurnedOff();
+	chrome.runtime.sendMessage({action: "tts-turnedOff"});
+	chrome.storage.local.set({'tts-status': 'off'}, function() {});
+}
 
 var audioAnalyser = ttsService.getAudioAnalyser();
 var frequencyData = new Uint8Array(audioAnalyser.frequencyBinCount);
@@ -42,4 +37,24 @@ setInterval(function(){
 	}
 },10);
 
-chrome.runtime.sendMessage({action: "tts-turnedOn"});
+//initial setting
+chrome.storage.local.get('tts-status', function(items) {
+	var status = items['tts-status'];
+	if(status == 'on') {
+		turnOn();
+	} else {
+		turnOff();
+	}
+});
+
+//receiving messages from cotnent script (to read) and popup (turnon/turnoff/getstatus)
+chrome.runtime.onMessage.addListener(
+	function(request, sender, sendResponse) {
+		console.log(request.action + " received");
+		switch(request.action) {
+			case("tts-read"): read(request.textToSpeech, request.languageOfDocument || navigator.language); break;
+			case("tts-turnOn"): turnOn(); break;
+			case("tts-turnOff"): turnOff(); break;
+			case("tts-getStatus"): turnedOn ? sendResponse({turnedOn:true}) : sendResponse({turnedOn:false}); break;
+		}
+	});
--- chrome/js/popup.js
+++ chrome/js/popup.js
@@ -1,18 +1,31 @@
-chrome.runtime.onMessage.addListener(
-	function(request, sender, sendResponse) {
-		switch(request.action) {
-			case("tts-turnedOn"):
-				document.body.className = "tts-on";
-				document.getElementById('onoffbutton').innerHTML = "turn off";
-				break;
-			case("tts-turnedOff"):
-				document.body.className = "tts-off";
-				document.getElementById('onoffbutton').innerHTML = "turn on";
-				break;
-		}
-	}
-);
+var turnedOn = null;
+
+function turnOn() {
+	turnedOn = true;
+	document.body.className = "tts-on";
+	document.getElementById('onoffbutton').innerHTML = "turn off";
+}
+
+function turnOff() {
+	turnedOn = false;
+	document.body.className = "tts-off";
+	document.getElementById('onoffbutton').innerHTML = "turn on";
+}
 
 document.getElementById('onoffbutton').addEventListener('click', function(){
-	chrome.runtime.sendMessage({action: "tts-turnOnOff"});
+	if(turnedOn) {
+		turnOff();
+		chrome.runtime.sendMessage({action: "tts-turnOff"});
+	} else {
+		turnOn();
+		chrome.runtime.sendMessage({action: "tts-turnOn"});
+	}
+});
+
+chrome.runtime.sendMessage({action: "tts-getStatus"}, function(response) {
+	if(response.turnedOn) {
+		turnOn();
+	} else {
+		turnOff();
+	}
 });
