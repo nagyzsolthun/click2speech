e5237e15afeeb002a4ad6175dcec6e3846c23edb
nagyzsolthun <nagy.zsolt.hun@gmail.com>
2014-10-19 20:01:01 +0200

    improve options (#4)

--- chrome/js/chrome/options.js
+++ chrome/js/chrome/options.js
@@ -3,7 +3,7 @@ require.config({
 });
 
 /** @param TestLines the text to show at the bottom of options page */
-require(["TestLines"], function(testLines) {
+require([], function() {
 	var speedNumber = document.getElementById("speedNumber");
 	var speedRange = document.getElementById("speedRange");
 	chrome.runtime.sendMessage({action: "tts-getSpeed"}, function(response) {
@@ -17,16 +17,5 @@ require(["TestLines"], function(testLines) {
 	speedRange.oninput = function() {
 		chrome.runtime.sendMessage({action: "tts-setSpeed",speed: this.value});
 		speedNumber.value = this.value;
-	}
-	
-	var textToRead = document.getElementById("textToRead");
-	textToRead.value = testLines[navigator.language];
-	
-	textToRead.onmousedown = function() {
-		chrome.runtime.sendMessage({
-			action: "tts-read",
-			textToSpeech: getSelection().toString(),
-			languageOfDocument: document.documentElement.lang
-		});
 	}
 });
