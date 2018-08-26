f85883716ee8e945eba5408a06767ee4c87cac4b
Zsolt Nagy <nagy.zsolt.hun@gmail.com>
2018-08-26 23:22:53 +0200

    selection settings

--- chrome/options/app/general-settings/general-settings.component.ts
+++ chrome/options/app/general-settings/general-settings.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 
 @Component({
   selector: 'app-general-settings',
@@ -7,9 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GeneralSettingsComponent implements OnInit {
 
-  constructor() { }
+  selectionOptions = [
+    { name: "hoverSelect", selected: false },
+    { name: "arrowSelect", selected: false },
+    { name: "browserSelect", selected: false },
+  ];
+
+  constructor(private changeDetectionRef: ChangeDetectorRef) { }
 
   ngOnInit() {
+
+    // get initial storage values
+    chrome.storage.local.get(null, settings => {
+      this.selectionOptions.forEach(option => option.selected = settings[option.name])
+      this.changeDetectionRef.detectChanges();
+    });
+
+    // subscribe on changes
+    chrome.storage.onChanged.addListener(changes => {
+      Object.keys(changes).forEach(name => {
+        const option = this.selectionOptions.find(option => option.name == name);
+        if (option) { option.selected = changes[name].newValue; }
+      });
+      this.changeDetectionRef.detectChanges();
+    })
+  }
+
+  select(name: string) {
+    const option = this.selectionOptions.find(option => option.name == name);
+    chrome.storage.local.set({ [name]: !option.selected });
   }
 
 }
