4d3430f6557c8a6dcda1091256a744edcc6001ba
Zsolt Nagy <nagy.zsolt.hun@gmail.com>
2019-11-10 20:42:58 +0100

    general settings progress

--- chrome/options/src/components/General.tsx
+++ chrome/options/src/components/General.tsx
@@ -1,8 +1,24 @@
 import React from 'react';
-import "./Header.css";
+import translate from '../translate';
+
+const selectionOptions = [
+  { name: "hoverSelect", selected: false },
+  { name: "arrowSelect", selected: false },
+  { name: "browserSelect", selected: false },
+];
+
+// TODO take settings from chrome.storage, maybe use useState ?
 
 const General = (props: any) => (
-  <h1>General</h1>
+  <div className="GeneralSettings setting hoverable">
+    <div>{"selectionOptions"}</div>
+    <ul className="choiceList selectionList">
+        {selectionOptions
+          .map(option => translate(option.name))
+          .map(name => <li><span>{name}</span></li>)
+        }
+    </ul>
+  </div>
 );
 
 export default General as React.FC;
